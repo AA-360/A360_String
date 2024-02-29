40e4d1af64d260d8c8f98a35ec491e518f36cdd4
Melque Lima <melque_ex@yahoo.com.br>
2024-02-28 22:03:49 -0300

    update

---
+++ build/generated/sources/annotationProcessor/java/main/com/automationanywhere/botcommand/samples/commands/basic/TextAreaAssignCommand.java
@@ -0,0 +1,63 @@
+package com.automationanywhere.botcommand.samples.commands.basic;
+
+import com.automationanywhere.bot.service.GlobalSessionContext;
+import com.automationanywhere.botcommand.BotCommand;
+import com.automationanywhere.botcommand.data.Value;
+import com.automationanywhere.botcommand.exception.BotCommandException;
+import com.automationanywhere.commandsdk.i18n.Messages;
+import com.automationanywhere.commandsdk.i18n.MessagesFactory;
+import java.lang.ClassCastException;
+import java.lang.Deprecated;
+import java.lang.Object;
+import java.lang.String;
+import java.lang.Throwable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class TextAreaAssignCommand implements BotCommand {
+  private static final Logger logger = LogManager.getLogger(TextAreaAssignCommand.class);
+
+  private static final Messages MESSAGES_GENERIC = MessagesFactory.getMessages("com.automationanywhere.commandsdk.generic.messages");
+
+  @Deprecated
+  public Optional<Value> execute(Map<String, Value> parameters, Map<String, Object> sessionMap) {
+    return execute(null, parameters, sessionMap);
+  }
+
+  public Optional<Value> execute(GlobalSessionContext globalSessionContext,
+      Map<String, Value> parameters, Map<String, Object> sessionMap) {
+    logger.traceEntry(() -> parameters != null ? parameters.entrySet().stream().filter(en -> !Arrays.asList( new String[] {}).contains(en.getKey()) && en.getValue() != null).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)).toString() : null, ()-> sessionMap != null ?sessionMap.toString() : null);
+    TextAreaAssign command = new TextAreaAssign();
+    HashMap<String, Object> convertedParameters = new HashMap<String, Object>();
+    if(parameters.containsKey("value") && parameters.get("value") != null && parameters.get("value").get() != null) {
+      convertedParameters.put("value", parameters.get("value").get());
+      if(convertedParameters.get("value") !=null && !(convertedParameters.get("value") instanceof String)) {
+        throw new BotCommandException(MESSAGES_GENERIC.getString("generic.UnexpectedTypeReceived","value", "String", parameters.get("value").get().getClass().getSimpleName()));
+      }
+    }
+    if(convertedParameters.get("value") == null) {
+      throw new BotCommandException(MESSAGES_GENERIC.getString("generic.validation.notEmpty","value"));
+    }
+
+    try {
+      Optional<Value> result =  Optional.ofNullable(command.action((String)convertedParameters.get("value")));
+      return logger.traceExit(result);
+    }
+    catch (ClassCastException e) {
+      throw new BotCommandException(MESSAGES_GENERIC.getString("generic.IllegalParameters","action"));
+    }
+    catch (BotCommandException e) {
+      logger.fatal(e.getMessage(),e);
+      throw e;
+    }
+    catch (Throwable e) {
+      logger.fatal(e.getMessage(),e);
+      throw new BotCommandException(MESSAGES_GENERIC.getString("generic.NotBotCommandException",e.getMessage()),e);
+    }
+  }
+}
